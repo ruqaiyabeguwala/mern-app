48fff4d852f18f9b410019a1205530de41512d34
Ruqaiya <Ruqaiya.beguwala@gmail.com>
2020-04-18 21:24:00 +0530

    removed mongoose

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
 
 const route = require("./routes/api/studentRoute");
 const app = express();
@@ -12,6 +11,6 @@ app.use(bodyParser.json());
 //router
 app.use("/api/student", route);
 
-//connect
+
 
 app.listen(5000, () => console.log("Running at port: " + 5000));
