f3ccc58b795f7b5796d913b6a207fbbe94c111a2
Ruqaiya <Ruqaiya.beguwala@gmail.com>
2020-04-18 21:08:34 +0530

    new server

--- server.js
+++ server.js
@@ -8,12 +8,6 @@ const app = express();
 //middleware bodyparser
 app.use(bodyParser.json());
 
-//connect to db
-var con = mongoose
-  .connect("mongodb://localhost/Student", {
-    useNewUrlParser: true
-  })
-  .then(() => console.log("Mongodb running"));
 
 //router
 app.use("/api/student", route);
